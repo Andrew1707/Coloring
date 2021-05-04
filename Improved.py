8c78b2f9a876007c440a3b523144d661028a490f
bluehue22 <benton.cheng@rutgers.edu>
2021-05-04 14:05:18 -0400

    outlined Improved.py

--- Improved.py
+++ Improved.py
@@ -11,6 +11,85 @@ import Color_Coded as cc
 ### values for each of the  k colors from get_clusters fxn
 
 
+# Returns dictionary of (pixel index:cluster center) pairs using Lloyd's k-means clustering algorithm
+# INPUTS: k is the number of clusters, data is a list of data RGB tuples
+def recolor(k, data):
+    # Pick random centers from the data list to start
+    centers = cc.random_picks(k, data)
+
+    # RSS is the residual sum of squares (total error squared of each point's dist to its cluster center)
+    old_RSS, RSS = 0, 0
+    centers_changed = True
+
+    while centers_changed:
+        # Create dictionary mapping data tuple locations to centers
+        dictionary = dict()
+        old_RSS = RSS
+        RSS = 0
+
+        # RGB distances are weighted evenly (using square norm for dist)
+        for i in range(len(data)):
+            # The index of the closest center to i
+            closest = 0
+            # Initialize distance between i and closest center
+            dist = float("inf")
+
+            # Iterate through centers
+            for c in range(0, k):
+                temp = 0
+                # Iterate through each R/G/B pixel value
+                for val in range(3):
+                    temp += (centers[c][val] - data[i][val]) ** 2
+                # If distance to center c is shorter
+                if temp < dist:
+                    dist = temp
+                    closest = c
+
+            dictionary.update({i: closest})
+
+        # Now adjust center locations
+        # cluster is a list of [count, avg] lists corresponding to the center at its index
+        cluster = []
+        # Initialize cluster structure
+        for i in range(k):
+            cluster.append([0, [0, 0, 0]])
+        # Sum up which color triplets belong to which cluster
+        for i in range(len(data)):
+            centers_index = dictionary.get(i)
+            cluster[centers_index][0] += 1
+
+            for rgb in range(3):
+                cluster[centers_index][1][rgb] += data[i][rgb]
+
+                # Update RSS
+                RSS += (centers[centers_index][rgb] - data[i][rgb]) ** 2
+        # Average out the [~, avg] part of each pair in cluster
+        for i in range(k):
+            for rgb in range(3):
+                if cluster[i][0] != 0:
+                    cluster[i][1][rgb] = int(cluster[i][1][rgb] / cluster[i][0])
+
+        # Updates centers
+        centers_changed = False
+        for i in range(k):
+            if centers[i] != cluster[i][1]:
+                centers[i] = cluster[i][1]
+                centers_changed = True
+
+        # Early termination condition (close enough to solution according to prev testing)
+        if abs(old_RSS - RSS) < (old_RSS * 0.0001):
+            centers_changed = False
+
+    # Replace centers indicies in dictionary with finalized colors
+    # rep_colors is copy of data except w/ RGB vals replaced w/ corresponding cluster colors
+    rep_colors = np.array(data)
+    for key in dictionary:
+        new_val = np.array(centers[dictionary.get(key)])
+        rep_colors[key] = new_val
+
+    return rep_colors
+
+
 def initialize_weights(k):
     weights = np.empty((9,k))
     for i in range(9):
@@ -20,8 +99,18 @@ def initialize_weights(k):
     return weights
 
 
-def color_classifier(weights, patch):
-    raw_preds = np.dot(patch, weights)
+# Loss calculator
+def loss():
+    return
+
+
+# Loss gradient calculator
+def loss_grad():
+    return
+
+
+def trainer(weights, patch):
+    raw_preds = np.cross(patch, weights)
 
     # Max portion here
     return
@@ -29,8 +118,8 @@ def color_classifier(weights, patch):
 
 def main():
     # Relative path
-    original = Image.open("C:Images\\nature.jpeg")
-
+    original = Image.open("C:Nature\small_nature.jpeg")
+    k = 5 #! Might not need
 
     img = original.convert("L")  # grayscale copy
     width, height = img.size
@@ -45,16 +134,24 @@ def main():
 
     # Convert to numpy array
     testing_as_list = np.array(list(testing.getdata()))
-    training_as_list = np.array(list(training.getdata()))
+    training_as_list = list(training.getdata())
     training_grayscale_as_list = np.array(list(training_grayscale.getdata()))
 
-    clusters = cc.get_clusters(5, training_as_list)
-    tuple_clusters = {}
-    for x, c in enumerate(clusters):
-        RGB = (clusters[c][0], clusters[c][1], clusters[c][2])
-        tuple_clusters.update({x: RGB})
-    
-    
+    # Run get clusters, get np array of RGB vals that replace the original RGBs in training_as_list
+    training_recolored = recolor(k, training_as_list)
+
+    # Append training arrays so cols 1-3 are recolored RGB, col 4 is corresponding grey val
+    training_grayscale_as_list = np.reshape(training_grayscale_as_list, (training_grayscale_as_list.size, 1))
+    combined_ds = np.append(training_recolored, training_grayscale_as_list, axis=1)
+
+    # Shuffle combined_ds, then split into train_ds and val_ds
+    np.random.shuffle(combined_ds)
+    train_ds = combined_ds[:,-1]
+    val_ds = combined_ds[:, :-1]
+
+    # Initalize weights
+    weights = initialize_weights(k) #! Might not need k, so reupdate
+
     return
 
 main()
