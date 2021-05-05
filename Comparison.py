2e26ecda298e804a6ae4a22cc13abf15388e783c
bluehue22 <benton.cheng@rutgers.edu>
2021-05-05 15:20:43 -0400

    Finalized Improved.py bot (it's amazing btw) & associated imgs

---
+++ Comparison.py
@@ -0,0 +1,70 @@
+import numpy as np
+import copy
+from PIL import Image
+import math
+import random
+import pathlib
+
+
+# Input img arrays must be same shape
+def loss_calc(img1, img2):
+    loss = 0
+    for i in range(len(img1)):
+        for j in range(len(img1[0])):
+            for k in range(len(img1[0][0])):
+                loss += (img1[i][j][k] - img2[i][j][k]) ** 2
+    return loss
+
+
+def main():
+    with Image.open("C:Nature\\right_basic.png") as basic, Image.open("C:Nature\\right_original.png") as original:
+        b_width, b_height = basic.size
+        o_width, o_height = original.size
+
+        basic_rgb = basic.convert(mode="RGB")
+        original_rgb = original.convert(mode="RGB")
+
+        basic_data = np.asarray(basic_rgb) / 255
+        original_data = np.asarray(original_rgb) / 255
+        
+        min_loss = float('inf')
+
+        x_variants = 6   # 418 to 423 endings
+        y_variants = 5   # 211 to 215 endings
+        for i in range(x_variants):
+            for j in range(y_variants):
+                loss = loss_calc(basic_data, original_data[0 + i : 418 + i, 0 + j : 211 + j, :])
+                print(f'Basic [{i}][{j}] loss: {loss}')
+                if loss < min_loss:
+                    min_loss = loss
+
+        print(f'Basic min_loss: {min_loss}')
+
+
+    with Image.open("C:Nature\\right_adv.png") as adv, Image.open("C:Nature\\right_original.png") as original:
+        a_width, a_height = adv.size
+        o_width, o_height = original.size
+
+        adv_rgb = adv.convert(mode="RGB")
+        original_rgb = original.convert(mode="RGB")
+
+        adv_data = np.asarray(adv_rgb) /255
+        original_data = np.asarray(original_rgb) /255
+        
+        min_loss = float('inf')
+
+        x_variants = 6   # 418 to 423 endings
+        y_variants = 5   # 211 to 215 endings
+        for i in range(x_variants):
+            for j in range(y_variants):
+                loss = loss_calc(adv_data, original_data[0 + i : 418 + i, 0 + j : 211 + j, :])
+                print(f'Adv [{i}][{j}] loss: {loss}')
+                if loss < min_loss:
+                    min_loss = loss
+
+        print(f'Adv min_loss: {min_loss}')
+
+
+# Basic min_loss: 1391.7868512108064
+# Adv min_loss: 11106.260638213464
+main()
--- Improved.py
+++ Improved.py
@@ -83,12 +83,13 @@ def recolor(k, data):
 
     # Replace centers indicies in dictionary with finalized colors
     # rep_colors is copy of data except w/ RGB vals replaced w/ corresponding cluster colors
+    centers = np.array(centers)
     rep_colors = np.array(data)
     for key in dictionary:
         new_val = np.array(centers[dictionary.get(key)])
         rep_colors[key] = new_val
 
-    return rep_colors, RSS
+    return rep_colors, RSS, centers
 
 
 def initialize_weights(k):
@@ -101,27 +102,78 @@ def initialize_weights(k):
 
 
 # Loss calculator
-def loss():
-    return
+def loss(softmaxed_arr, index):
+    return -1 * math.log10(softmaxed_arr[index])
 
 
 # Loss gradient calculator
-def loss_grad():
-    return
+def softmax_loss_grad(softmaxed_arr, val_arr):
+    grad = softmaxed_arr - val_arr
+    return grad
 
 
-def trainer(weights, patch):
-    raw_preds = np.cross(patch, weights)
+# Returns softmaxed array (adjusted implementation to prevent overflow)
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
 
-    # Max portion here
-    return
+
+def improvedModel(weights, patch):
+    raw_preds = np.matmul(patch, weights)
+    softmaxed_arr = softmax(raw_preds)
+    return softmaxed_arr
+
+
+def update_weights(lr, weights, patch, soft_gradient):
+    for i in range(len(weights[0])):
+        weights[:,i] = weights[:,i] - (lr * soft_gradient[i] * patch)
+    return weights
+
+
+# Trainer
+def train(weights, train_ds, val_ds, patch_ds, height, width, centers):
+    # Initialize learning rate (lr)
+    lr = 1e5
+    
+    # Call model and update_weights here
+    for i in range(len(patch_ds)):
+        train_patch = patch_ds[i]
+
+        # Get predictions through model
+        predictions = improvedModel(weights, train_patch)
+
+        # val_ds[i] is corresponding RGB array, make one hot encoded arr (validation array)
+        one_hot_encoded_arr = np.zeros(weights.shape[1])
+        one_index = 0
+        for j in range(len(centers)):
+            if np.array_equal(centers[j], val_ds[i]):
+                one_index = j
+                break
+        one_hot_encoded_arr[one_index] = 1
+
+        # Forced learning rate decay
+        if i == int(len(train_ds)/4):
+            lr = 1e4
+        elif i == int(len(train_ds)/2):
+            lr = 1e3
+
+        # Update weights
+        # loss_val = loss(predictions, one_index) #!!!!
+        # print(f'i: {i}, loss_val: {loss_val}')#! Could keep for plotting
+        soft_gradient = softmax_loss_grad(predictions, one_hot_encoded_arr)
+        weights = update_weights(lr, weights, train_patch, soft_gradient)
+
+        if (i % 1000) == 0: #!!!!!!!
+            print(f'Train i: {i}')# , Loss = {loss_val}') #!!!!!!!
+    
+    return weights
 
 
 def start():
     # Relative path
-    original = Image.open("C:Nature\small_nature.jpeg")
+    original = Image.open("C:Nature\\small_nature.jpeg")
     # original = Image.open("C:Images\EvenSmaller.png")
-    k = 5  #! Might not need
+    k = 10  # Adjusted number of clusters
 
     img = original.convert("L")  # grayscale copy
     width, height = img.size
@@ -130,42 +182,109 @@ def start():
     training = original.crop((1, 1, (width / 2) + 1, height + 1))
     testing = img.crop(((width / 2), 1, width, height + 1))  #!plus 1 to get them to equal size
     training_grayscale = img.crop((1, 1, (width / 2) + 1, height + 1))
+    testing.show()
+    original.close()
 
     # need sizes of all halves
     half_width, half_height = training.size
 
     # Convert to numpy array
     testing_as_list = np.array(list(testing.getdata()))
-    training_as_list = list(training.getdata())
+    training_as_list = list(training.getdata()) #! Do not convert to np array
     training_grayscale_as_list = np.array(list(training_grayscale.getdata()))
 
     # Run get clusters, get np array of RGB vals that replace the original RGBs in training_as_list
-    RSS_list = []
-    num_list = []
-    for num in range(1, 20):
-        print(num)
-        training_recolored, RSS = recolor(num, training_as_list)
-        RSS_list.append(RSS)
-        num_list.append(num)
-    plt.xlabel("k clusters")
-    plt.ylabel("RSS")
-    plt.plot(num_list, RSS_list, color="red", label="Smart")
-    plt.legend(loc="best")
-    plt.show()
-    return
+    training_recolored, _, centers = recolor(k, training_as_list)
+    
+    # # Code for RSS vs k plot
+    # RSS_list = []
+    # num_list = []
+    # for num in range(1, 20):
+    #     print(num)
+    #     training_recolored, RSS = recolor(num, training_as_list)
+    #     RSS_list.append(RSS)
+    #     num_list.append(num)
+    # plt.xlabel("k clusters")
+    # plt.ylabel("RSS")
+    # plt.plot(num_list, RSS_list, color="red", label="Smart")
+    # plt.legend(loc="best")
+    # plt.show()
+    # return
 
-    # Append training arrays so cols 1-3 are recolored RGB, col 4 is corresponding grey val
-    training_grayscale_as_list = np.reshape(training_grayscale_as_list, (training_grayscale_as_list.size, 1))
-    combined_ds = np.append(training_recolored, training_grayscale_as_list, axis=1)
 
-    # Shuffle combined_ds, then split into train_ds and val_ds
-    np.random.shuffle(combined_ds)
-    train_ds = combined_ds[:, -1]
-    val_ds = combined_ds[:, :-1]
+    # Append training arrays so cols 1-3 are recolored RGB
+    # col 4 is corresponding grey val
+    # could make a col 5 w/ added 3rd dimension for entire arr, 9 deep (for patches)
+    # skim the top surface (3rd dim 0th index) for cols 1-4, grab col 5 and reshape
+    combined_obj = np.zeros((training_grayscale_as_list.size, 5, 9))
+    combined_obj[:, :3, 0] = training_recolored
+    combined_obj[:, 3, 0] = training_grayscale_as_list
+    
+    # Add greyscale patches to combined_obj
+    for i in range(len(training_grayscale_as_list)):
+        combined_obj[i, 4, :] = np.array(cc.get3x3(training_grayscale_as_list, half_height, half_width, i))
+    
+    # Clear out all edges (no corresponding 3x3 patch)
+    i = 0
+    stop_index = len(training_grayscale_as_list)
+    while i < stop_index:
+        if math.isnan(combined_obj[i, 4, 0]):
+            combined_obj = np.delete(combined_obj, i, 0)
+            i -= 1
+            stop_index -= 1
+        i += 1
+
+    print(training_recolored.shape, training_grayscale_as_list.shape)#!!!!
+    print(f'combined shape: {combined_obj.shape}')#!!!
+    
+    # Rescale vals from [0, 255] to [0, 1], shuffle combined_ds, then split
+    combined_obj = combined_obj / 255
+    centers = centers / 255
+
+    np.random.shuffle(combined_obj)
+    train_ds = combined_obj[:, 3, 0]
+    val_ds = combined_obj[:, :3, 0]
+    patch_ds = combined_obj[:, 4, :]
+
 
     # Initalize weights
-    weights = initialize_weights(k)  #! Might not need k, so reupdate
+    weights = initialize_weights(k)
 
+    print(train_ds.shape, val_ds.shape, patch_ds.shape, weights.shape, centers.shape) #!!!!
+    print(f'centers: {centers}')#!!!!
+
+    weights = train(weights, train_ds, val_ds, patch_ds, half_width, half_height, centers)
+
+    #! TRACKER #!!!!!
+    color_dist = np.zeros(10)#!!!
+
+    # Apply to test image
+    test_rgb = np.zeros((testing_as_list.size,3))
+    for i in range(len(testing_as_list)):
+        if (i % 1000) == 0: #!!!!!!!
+            print(f'Final Test i: {i}') #!!!!!!!
+        temp_patch = np.array(cc.get3x3(testing_as_list, half_height, half_width, i))
+        
+        if temp_patch.size == 1:
+            continue
+        
+        temp_patch = temp_patch / 255
+
+        temp_pred = improvedModel(weights, temp_patch)
+        new_color = centers[np.argmax(temp_pred)]
+        color_dist[np.argmax(temp_pred)] += 1 #!!!
+        test_rgb[i] = new_color
+
+    # Rescale new RGB image back up to [0, 255] and display
+    test_rgb = test_rgb * 255
+    test_rgb = np.reshape(test_rgb, (half_height, half_width, 3))
+
+    print(f'color_dist: {color_dist}') #!!!
+
+    new_img = Image.fromarray(np.uint8(test_rgb), mode='RGB')
+    new_img.show()
+    
+    new_img.save("C:Nature\\new_img.jpg")
     return
 
 
